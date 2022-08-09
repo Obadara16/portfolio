b5d6263a48b347037b73b7ec7b88c19d6bf99ede
Obadara16 <obadararoqeeb@gmail.com>
2022-08-09 16:14:24 +0100

    github links added

--- src/data.js
+++ src/data.js
@@ -5,26 +5,27 @@ import IMG3 from './assets/portfolio3.png'
 export const data = [
     {
         id: 1,
-        image: IMG1,
-        title: 'Estate Chain',
-        description: 'Estate chain is a real estate platform that aims to change real estate in Africa by keeping track of properties, verifying them, and purchasing and selling them. All of this will be accomplished through the use of blockchain technology.',
-        github: 'https://github.com/Obadara16',
-        demo: 'https://herokuapp.com'
+        image: IMG3,
+        title: 'Brocon',
+        description: 'An organization of undergraduate student consultants working to provide quality, pro-bono advisory services for our clients. We work with startup and growing companies from a variety of industries, ranging from technology to healthcare to fashion, providing innovative and effective solutions.',
+        github: 'https://github.com/Obadara16/Brocon',
+        demo: 'https://roqbrocon.herokuapp.com'
     },
     {
         id: 2,
+        image: IMG1,
+        title: 'Estate Chain',
+        description: 'Estate chain is a real estate platform that aims to change real estate in Africa by keeping track of properties, verifying them, and purchasing and selling them. All of this will be accomplished through the use of blockchain technology. ',
+        github: 'https://github.com/Obadara16',
+        demo: 'In Development'
+    },
+    {
+        id: 3,
         image: IMG2,
         title: 'Estate Chain Awaiting Page',
         description: 'Estate chain is a real estate platform that aims to change real estate in Africa by keeping track of properties, verifying them, and purchasing and selling them. All of this will be accomplished through the use of blockchain technology.',
         github: 'https://github.com/Obadara16',
-        demo: 'https://herokuapp.com'
-    },
-    {
-        id: 3,
-        image: IMG3,
-        title: 'Brocon',
-        description: 'An organization of undergraduate student consultants working to provide quality, pro-bono advisory services for our clients. We work with startup and growing companies from a variety of industries, ranging from technology to healthcare to fashion, providing innovative and effective solutions.',
-        github: 'https://github.com/Obadara16',
-        demo: 'https://herokuapp.com'
+        demo: 'Not Hosted Yet'
     }
+    
 ]
