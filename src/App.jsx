cdd306780bfe0a4dd5c3ca782db48893ca719b7a
Obadara16 <obadararoqeeb@gmail.com>
2023-04-28 22:43:21 +0100

    feat:: links modified

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,14 @@ import Portfolio from './components/Portfolio'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 import ScrollToTop from './components/ScrollToTop'
+import sanityClient from '@sanity/client'
+
+const client = sanityClient({
+  projectId: 'your_project_id_here',
+  dataset: 'your_dataset_name_here',
+  useCdn: true // Enable caching
+})
+
 
 const App = () => {
   return (
--- src/data.js
+++ src/data.js
@@ -1,58 +1,63 @@
-import IMG1 from './assets/afriqhub.png'
-import IMG2 from './assets/afriqfabs.png'
-import IMG3 from './assets/gainsborough.png'
-import IMG4 from './assets/portfolio.png'
-import IMG5 from './assets/portfolio3.png'
-import IMG6 from './assets/bti.png'
+import IMG1 from "./assets/afriqhub.png";
+import IMG2 from "./assets/afriqfabs.png";
+import IMG3 from "./assets/gainsborough.png";
+import IMG4 from "./assets/portfolio.png";
+import IMG5 from "./assets/portfolio3.png";
+import IMG6 from "./assets/bti.png";
 
 export const data = [
-    {
-        id: 1,
-        image: IMG1,
-        title: 'AfriqHub',
-        description: "Afriqhub is a platform that aims to improve users' knowledge on various topics while allowing them to connect with like-minded people across Africa. The platform provides premium content that keeps users engaged and allows them to establish a sense of community.",
-        github: 'https://github.com/Obadara16',
-        demo: 'https://afriqhub.com'
-    },
-    {
-        id: 2,
-        image: IMG2,
-        title: 'AfriqFabs',
-        description: 'AfriqFabs is a family-owned business that provides high-quality, stylish, and practical fabrics at affordable prices. They offer a wide range of fabrics, including cotton, silk, polyester, rayon, linen, and wool, and are committed to sustainable and ethical sourcing practices.  ',
-        github: 'https://github.com/Obadara16/afriqfabs',
-        demo: 'https://afriqfabs.onrender.com'
-    },
-    {
-        id: 3,
-        image: IMG3,
-        title: 'GainsBorough',
-        description: 'Gainsborough is a real estate platform that provides users with the ability to buy, rent, and lease properties. The platform also allows real estate agents to upload and sell properties to potential buyers and renters.',
-        github: 'https://github.com/Obadara16/',
-        demo: 'https://blockchainlabacademy.com'
-    },
-    {
-        id: 4,
-        image: IMG4,
-        title: 'Brocon',
-        description: 'An organization of undergraduate student consultants working to provide quality, pro-bono advisory services for our clients. We work with startup and growing companies from a variety of industries, ranging from technology to healthcare to fashion, providing innovative and effective solutions.',
-        github: 'https://github.com/Obadara16/Brocon',
-        demo: 'https://brocon.onrender.com'
-    },
-    {
-        id: 5,
-        image: IMG5,
-        title: 'Estate Chain',
-        description: 'Estate chain is a real estate platform that aims to change real estate in Africa by keeping track of properties, verifying them, and purchasing and selling them. All of this will be accomplished through the use of blockchain technology. ',
-        github: 'https://github.com/Obadara16',
-        demo: 'In Development'
-    },
-    {
-        id: 6,
-        image: IMG6,
-        title: 'BlockchainLab Academy',
-        description: 'Blockchain Lab Academy is an online learning platform that offers courses and training on blockchain technology. The platform provides comprehensive and up-to-date content on various blockchain topics, including blockchain basics, smart contracts, decentralized finance (DeFi), and more.',
-        github: 'https://github.com/Obadara16/',
-        demo: 'https://blockchainlabacademy.com'
-    }
-    
-]
+  {
+    id: 1,
+    image: IMG1,
+    title: "AfriqHub",
+    description:
+      "Afriqhub is a platform that aims to improve users' knowledge on various topics while allowing them to connect with like-minded people across Africa. The platform provides premium content that keeps users engaged and allows them to establish a sense of community.",
+    github: "https://github.com/Obadara16",
+    demo: "https://afriqhub.com",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "AfriqFabs",
+    description:
+      "AfriqFabs is a family-owned business that provides high-quality, stylish, and practical fabrics at affordable prices. They offer a wide range of fabrics, including cotton, silk, polyester, rayon, linen, and wool, and are committed to sustainable and ethical sourcing practices.  ",
+    github: "https://github.com/Obadara16/afriqfabs",
+    demo: "https://afriqfabs.onrender.com",
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "GainsBorough",
+    description:
+      "Gainsborough is a real estate platform that provides users with the ability to buy, rent, and lease properties. The platform also allows real estate agents to upload and sell properties to potential buyers and renters.",
+    github: "https://github.com/Obadara16/",
+    demo: "https://roqportfolio.onrender.com",
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: "Brocon",
+    description:
+      "An organization of undergraduate student consultants working to provide quality, pro-bono advisory services for our clients. We work with startup and growing companies from a variety of industries, ranging from technology to healthcare to fashion, providing innovative and effective solutions.",
+    github: "https://github.com/Obadara16/Brocon",
+    demo: "https://brocon.onrender.com",
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: "Estate Chain",
+    description:
+      "Estate chain is a real estate platform that aims to change real estate in Africa by keeping track of properties, verifying them, and purchasing and selling them. All of this will be accomplished through the use of blockchain technology. ",
+    github: "https://github.com/Obadara16",
+    demo: "https://roqportfolio.onrender.com",
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: "BlockchainLab Academy",
+    description:
+      "Blockchain Lab Academy is an online learning platform that offers courses and training on blockchain technology. The platform provides comprehensive and up-to-date content on various blockchain topics, including blockchain basics, smart contracts, decentralized finance (DeFi), and more.",
+    github: "https://github.com/Obadara16/",
+    demo: "https://blockchainlabacademy.com",
+  },
+];
